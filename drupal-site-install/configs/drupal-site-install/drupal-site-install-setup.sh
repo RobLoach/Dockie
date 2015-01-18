52bcded085f973d997c0237e4c97a3f88a261ac2
Benedikt Forchhammer <b.forchhammer@gmail.com>
2015-01-18 16:00:01 +0100

    Install on mysql instead of pqsql.

--- drupal-site-install/configs/drupal-site-install/drupal-site-install-setup.sh
+++ drupal-site-install/configs/drupal-site-install/drupal-site-install-setup.sh
@@ -10,13 +10,14 @@ chmod 777 -R sites/default/files
 
 # Set up the Settings files
 cp -f sites/default/default.settings.php sites/default/settings.php
+cp -f example.gitignore .gitignore
 chmod 777 sites/default/settings.php
 [[ -f sites/default/default.services.yml ]] && cp -f sites/default/default.services.yml sites/default/services.yml
 [[ -f sites/default/services.yml ]] && chmod 777 sites/default/services.yml
 
 # Install the site with Drush
-# @todo Use MySQL instead of SQLite.
 /root/.composer/vendor/bin/drush site-install ${PROFILE} --yes \
-  --db-url=sqlite://sites/default/files/.ht.sqlite \
+  --db-url=mysql://root:root@localhost/drupal \
+  --db-su=root --db-su-pw=root \
   --account-name="admin" \
   --account-pass="admin"
